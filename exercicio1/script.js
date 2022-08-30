ebc17f7238048f40f0ade8897a4acf32120eaf06
Jessica <jessica.thamiresnunes@gmail.com>
2022-08-30 13:56:17 -0300

    Realizado com sucesso

---
+++ exercicio1/script.js
@@ -0,0 +1,11 @@
+
+const numeroPar = Number(prompt("digite um número par"))
+
+console.log("Número digitado:", numeroPar)
+console.log("Resto do número digitado:", numeroPar%2)
+
+
+//Quando se é acrescentado números pares, sendo dividido por 2, o resto é sempre 0.
+
+
+// Terá resultado diferentes de 0.
