d357188c8d69ae2bfc0c3a93f577e5bc9eb25a0b
Jessica <jessica.thamiresnunes@gmail.com>
2022-08-30 15:39:04 -0300

    Exercícios 1 ao 3 realizados

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,4 +1,4 @@
-const console.log(5>20&&20<2)
-const console.log(5===5||5==="5")
-const console.log!(20>50)
-const console.log!(20>50||50>60)
+console.log(5>20&&5<2)
+console.log(5===5||5==="5")
+console.log (!(20>50))
+console.log (!(20>50||50>60))
